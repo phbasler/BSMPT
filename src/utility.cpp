042d0f0e448ac2865ebc98fd7be134dbd9bb6437
Philipp Basler <philipp.basler@kit.edu>
2020-05-04 21:26:35 +0200

    Added the utility.cpp

--- src/utility.cpp
+++ src/utility.cpp
@@ -2,14 +2,26 @@
  * utility.h
  *
  *  Copyright (C) 2020  Philipp Basler, Margarete Mühlleitner and Jonas Müller
+<<<<<<< HEAD
+=======
+
+>>>>>>> Added the utility.cpp
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
         the Free Software Foundation, either version 3 of the License, or
         (at your option) any later version.
+<<<<<<< HEAD
+=======
+
+>>>>>>> Added the utility.cpp
         This program is distributed in the hope that it will be useful,
         but WITHOUT ANY WARRANTY; without even the implied warranty of
         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
         GNU General Public License for more details.
+<<<<<<< HEAD
+=======
+
+>>>>>>> Added the utility.cpp
         You should have received a copy of the GNU General Public License
         along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,6 +41,10 @@ std::ostream& operator<<(std::ostream& os, const ModelID::ModelIDs& Model)
     static auto IMN = BSMPT::ModelID::InvertModelNames();
     os << IMN.at(Model);
     return os;
+}
+bool StringStartsWith(const std::string& str, const std::string& prefix)
+{
+    return str.size() >= prefix.size() and str.find(prefix) == 0;
 }
 
 }
