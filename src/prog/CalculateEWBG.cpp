aa32a10a8430d68a50139d296b76899d989eb9d3
Philipp Basler <philipp.basler@kit.edu>
2020-05-05 19:13:35 +0200

    Additional CLI options for BSMPT, CalcCT, CreateMuGrid and PlotEWBG_mu

--- src/prog/CalculateEWBG.cpp
+++ src/prog/CalculateEWBG.cpp
@@ -45,25 +45,123 @@ using namespace std;
 using namespace BSMPT;
 
 
+auto getCLIArguments(int argc, char *argv[])
+{
+    struct ReturnType{
+        BSMPT::ModelID::ModelIDs Model{};
+        int FirstLine{}, LastLine{};
+        std::string InputFile, OutputFile,ConfigFile;
+        bool TerminalOutput{false};
+        double vw{0.1};
+    };
+
+    std::vector<std::string> args;
+    for(int i{1};i<argc;++i) args.push_back(argv[i]);
+
+    if(argc < 7 or args.at(0) == "--help")
+    {
+        int SizeOfFirstColumn = std::string("--TerminalOutput=           ").size();
+        std::cout << "CalculateEWBG calculates the strength of the electroweak phase transition" << std::endl
+                  << "It is called either by " << std::endl
+                  << "./CalculateEWBG model input output FirstLine LastLine ConfigFile" << std::endl
+                  << "or with the following arguments" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<< "--help"
+                  << "Shows this menu" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--model="
+                  << "The model you want to investigate"<<std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--input="
+                  << "The input file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--output="
+                  << "The output file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--FirstLine="
+                  <<"The first line in the input file to calculate the EWPT. Expects line 1 to be a legend." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--LastLine="
+                  <<"The last line in the input file to calculate the EWPT." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--config="
+                  << "The EWBG config file." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--TerminalOutput="
+                  <<"y/n Turns on additional information in the terminal during the calculation." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--vw="
+                  << "Wall velocity for the EWBG calculation. Default value of 0.1." << std::endl;
+        ShowInputError();
+    }
+
+    if(args.size() > 0 and args.at(0)=="--help")
+    {
+        throw int{0};
+    }
+    else if(argc < 7)
+    {
+        throw std::runtime_error("Too few arguments.");
+    }
+
+
+    ReturnType res;
+    std::string prefix{"--"};
+    bool UsePrefix = StringStartsWith(args.at(0),prefix);
+    if(UsePrefix)
+    {
+        for(const auto& arg: args)
+        {
+            auto el = arg;
+            std::transform(el.begin(), el.end(), el.begin(), ::tolower);
+            if(StringStartsWith(el,"--model="))
+            {
+                res.Model = BSMPT::ModelID::getModel(el.substr(std::string("--model=").size()));
+            }
+            else if(StringStartsWith(el,"--input="))
+            {
+                res.InputFile = arg.substr(std::string("--input=").size());
+            }
+            else if(StringStartsWith(el,"--output="))
+            {
+                res.OutputFile = arg.substr(std::string("--output=").size());
+            }
+            else if(StringStartsWith(el,"--firstline="))
+            {
+                res.FirstLine = std::stoi(el.substr(std::string("--firstline=").size()));
+            }
+            else if(StringStartsWith(el,"--lastline="))
+            {
+                res.LastLine = std::stoi(el.substr(std::string("--lastline=").size()));
+            }
+            else if(StringStartsWith(el,"--terminaloutput="))
+            {
+                res.TerminalOutput = el.substr(std::string("--lastline=").size()) == "y";
+            }
+            else if(StringStartsWith(el,"--vw="))
+            {
+                res.vw = std::stod(el.substr(std::string("--vw=").size()));
+            }
+            else if(StringStartsWith(el,"--config="))
+            {
+                res.ConfigFile = arg.substr(std::string("--config").size());
+            }
+        }
+    }
+    else{
+        res.Model = ModelID::getModel(args.at(0));
+        res.InputFile = args.at(1);
+        res.OutputFile = args.at(2);
+        res.FirstLine = std::stoi(args.at(3));
+        res.LastLine = std::stoi(args.at(4));
+        res.ConfigFile = args.at(5);
+        if(argc == 8) {
+            std::string s7 = argv[6];
+            res.TerminalOutput = ("y" == s7);
+        }
+    }
+
+
+    return res;
+}
+
 
 int main(int argc, char *argv[]) try{
 
+    const auto args = getCLIArguments(argc,argv);
 
-	// Setting the default value of 0.1 for the wall velocity
-	//TODO: vw --> As additional input read parameter 
-	const double vw = 0.1;
-
-
-	if((argc != 7) and (argc != 8 ))
-	{
-		std::cerr << "./CalculateEWBG Model Inputfile Outputfile  LineStart LineEnd ConfigFile TerminalOutput\n";
-		ShowInputError();
-		return EXIT_FAILURE;
-	}
-
-
-    auto Model=ModelID::getModel(argv[1]);
-    if(Model==ModelID::ModelIDs::NotSet) {
+    if(args.Model==ModelID::ModelIDs::NotSet) {
         std::cerr << "Your Model parameter does not match with the implemented Models." << std::endl;
         ShowInputError();
         return EXIT_FAILURE;
@@ -71,57 +169,41 @@ int main(int argc, char *argv[]) try{
 
 
 //Init: Interface Class for the different transport methods 
-    Baryo::CalculateEtaInterface EtaInterface(argv[6] /* = Config file */);
-//Begin: Input Read parameters
-	double LineStart,LineEnd;
-	char* in_file;char* out_file;
+    Baryo::CalculateEtaInterface EtaInterface(args.ConfigFile);
 
-	in_file = argv[2];
-	out_file = argv[3];
 
-	LineStart = atoi(argv[4]);
-	LineEnd = atoi(argv[5]);
-
-	bool TerminalOutput = false;
-	if(argc == 8) {
-		std::string s7 = argv[7];
-		std::cout << s7 << std::endl;
-		TerminalOutput = ("y" == s7);
-
-	}
-
-	if(LineStart < 1)
+    if(args.FirstLine < 1)
 	{
 		std::cout << "Start line counting with 1" << std::endl;
 		return EXIT_FAILURE;
 	}
-	if(LineStart > LineEnd)
+    if(args.FirstLine > args.LastLine)
 	{
 		std::cout << "LineEnd is smaller then LineStart " << std::endl;
 		return EXIT_FAILURE;
 	}
 
 	int linecounter = 1;
-	std::ifstream infile(in_file);
+    std::ifstream infile(args.InputFile);
 	if(!infile.good()) 
 	{
 		std::cout << "Input file not found " << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	std::ofstream outfile(out_file);
+    std::ofstream outfile(args.OutputFile);
 	if(!outfile.good())
 	{
-		std::cout << "Can not create file " << out_file << std::endl;
+        std::cout << "Can not create file " << args.OutputFile << std::endl;
 		return EXIT_FAILURE;
 	}
 	std::string linestr;
-    std::shared_ptr<Class_Potential_Origin> modelPointer = ModelID::FChoose(Model);// Declare the model pointer with the necessary parameters
+    std::shared_ptr<Class_Potential_Origin> modelPointer = ModelID::FChoose(args.Model);// Declare the model pointer with the necessary parameters
     std::vector<std::string> etaLegend = EtaInterface.legend();// Declare the vector for the PTFinder algorithm
 //Begin: Input Read
 	while(getline(infile,linestr))
 	{
-		if(linecounter > LineEnd) break;
+        if(linecounter > args.LastLine) break;
 		else if(linecounter == 1)
 		{
 			// Write legend
@@ -140,24 +222,24 @@ int main(int argc, char *argv[]) try{
             for(auto x: etaLegend) outfile << sep << x;
 		    outfile << std::endl;
 		}
-		else if(linecounter >= LineStart and linecounter <= LineEnd and linecounter != 1)
+        else if(linecounter >= args.FirstLine and linecounter <= args.LastLine and linecounter != 1)
 		{
-			if(TerminalOutput)
+            if(args.TerminalOutput)
 			{
 				std::cout << "Currently at line " << linecounter << std::endl;
 			}
 //Begin: Parameter Set Up for BSMPT
 			modelPointer->initModel(linestr);
 			modelPointer->FindSignSymmetries();
-			if(LineStart == LineEnd ) modelPointer->write();
-            if(TerminalOutput) std::cout<<"Calling PTFinder"<<std::endl;
+            if(args.FirstLine == args.LastLine ) modelPointer->write();
+            if(args.TerminalOutput) std::cout<<"Calling PTFinder"<<std::endl;
 
 //Call: BSMPT 
             auto EWPT = Minimizer::PTFinder_gen_all(modelPointer,0,300);
 //Define parameters for eta
 			std::vector<double> eta;
             if(EWPT.StatusFlag == Minimizer::MinimizerStatus::SUCCESS and C_PT*EWPT.Tc < EWPT.vc){
-                if(TerminalOutput) std::cout<<"SFOEWPT found..."<<std::endl;
+                if(args.TerminalOutput) std::cout<<"SFOEWPT found..."<<std::endl;
 				std::vector<double> vcritical,vbarrier;
                 vcritical = EWPT.EWMinimum;
                 double TC = EWPT.Tc;
@@ -167,15 +249,15 @@ int main(int argc, char *argv[]) try{
                 for(size_t i=0;i<modelPointer->get_nVEV();i++) startpoint.push_back(0.5*vcritical.at(i));
                 vevsymmetricSolution=Minimizer::Minimize_gen_all(modelPointer,TC+1,checksym,startpoint);
 //Call: Calculation of eta in the different implemented approaches 
-                if(TerminalOutput) std::cout<<"Calling CalcEta..."<<std::endl;
-				eta = EtaInterface.CalcEta(vw,vcritical,vevsymmetricSolution,TC,modelPointer);
+                if(args.TerminalOutput) std::cout<<"Calling CalcEta..."<<std::endl;
+                eta = EtaInterface.CalcEta(args.vw,vcritical,vevsymmetricSolution,TC,modelPointer);
 //Outfile
 				outfile << linestr;
                 for(auto x:modelPointer->get_parCTStored()) outfile << sep << x;
                 outfile << sep << EWPT.Tc << sep << EWPT.vc;
                 outfile << sep << EWPT.vc / EWPT.Tc;
                 for(auto x: EWPT.EWMinimum) outfile << sep << x;
-                outfile << sep << vw;
+                outfile << sep << args.vw;
                 outfile << sep << EtaInterface.getLW();
                 outfile << sep << EtaInterface.GSL_integration_mubl_container.getSymmetricCPViolatingPhase_top();
                 outfile << sep << EtaInterface.GSL_integration_mubl_container.getBrokenCPViolatingPhase_top();
@@ -192,7 +274,7 @@ int main(int argc, char *argv[]) try{
                 outfile << sep << EWPT.Tc << sep << EWPT.vc;
                 for(auto x: EWPT.EWMinimum) outfile << sep << x;
                 outfile << sep << EWPT.vc / EWPT.Tc;
-                outfile << sep << vw;
+                outfile << sep << args.vw;
                 outfile << sep << -1; //LW
                 outfile << sep << -50;//top sym CP phase
                 outfile << sep << -50;//top brk CP phase
@@ -204,7 +286,7 @@ int main(int argc, char *argv[]) try{
 				outfile << std::endl;
 			}//END: No SFOEWPT
 
-			if(LineStart == LineEnd) {
+            if(args.FirstLine == args.LastLine) {
                 auto dimensionnames = modelPointer->addLegendTemp();
                 std::cout << "Succeded ? " << static_cast<int>(EWPT.StatusFlag)
                           << sep << " (1 = Success , -1 = v/T reached a value below " << C_PT << " during the calculation) \n";
@@ -228,11 +310,15 @@ int main(int argc, char *argv[]) try{
 		linecounter++;
 		if(infile.eof()) break;
 	}//END: Input Read
-	if(TerminalOutput) std::cout << std::endl;
+    if(args.TerminalOutput) std::cout << std::endl;
 //Closing & Free
 	outfile.close();
 	return EXIT_SUCCESS;
 }//END: Try
+catch(int)
+{
+    return EXIT_SUCCESS;
+}
 catch(exception& e){
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
--- src/prog/PlotEWBG_mu.cpp
+++ src/prog/PlotEWBG_mu.cpp
@@ -36,74 +36,163 @@
 #include <BSMPT/baryo_calculation/CalculateEtaInterface.h>
 #include <BSMPT/utility.h>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 
 using namespace std;
 using namespace BSMPT;
 
 
-int main(int argc, char *argv[]) try{
+auto getCLIArguments(int argc, char *argv[])
+{
+    struct ReturnType{
+        BSMPT::ModelID::ModelIDs Model{};
+        int Line{}, NumberOfSteps{};
+        std::string InputFile, OutputFile,ConfigFile;
+        bool TerminalOutput{false};
+        double vw{0.1};
+    };
 
-    double vw = 0.1; //Change to generic inputparameter
-    /**
-     * PrintErrorLines decides if parameter points with no valid EWPT (no NLO stability or T=300 vanishing VEV)
-     * are printed in the output file
-     */
-//    bool PrintErrorLines=true;
+    std::vector<std::string> args;
+    for(int i{1};i<argc;++i) args.push_back(argv[i]);
 
-    if(!( argc == 7 or argc == 8) )
+    if(argc < 7 or args.at(0) == "--help")
     {
-        std::cerr << "./RenormScale Model Inputfile Outputfile  Line NumberOfSteps Configfile TerminalOutput(y)\n";
+        int SizeOfFirstColumn = std::string("--TerminalOutput=           ").size();
+        std::cout << "EWBGRenormScale calculates the strength of the EWBG while varying the MSBar renormalisation scale" << std::endl
+                  << "It is called either by " << std::endl
+                  << "./EWBGRenormScale Model Inputfile Outputfile  Line NumberOfSteps Configfile" << std::endl
+                  << "or with the following arguments" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<< "--help"
+                  << "Shows this menu" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--model="
+                  << "The model you want to investigate"<<std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--input="
+                  << "The input file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--output="
+                  << "The output file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--Line="
+                  <<"The line in the input file with the parameter point. Expects line 1 to be a legend." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--config="
+                  << "The EWBG config file." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--TerminalOutput="
+                  <<"y/n Turns on additional information in the terminal during the calculation." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--vw="
+                  << "Wall velocity for the EWBG calculation. Default value of 0.1." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--NumberOfSteps="
+                  << "Number of Steps to vary the scale between 0.5 and 1.5 times the original scale." << std::endl;
         ShowInputError();
-        return EXIT_FAILURE;
+    }
+
+    if(args.size() > 0 and args.at(0)=="--help")
+    {
+        throw int{0};
+    }
+    else if(argc < 7)
+    {
+        throw std::runtime_error("Too few arguments.");
     }
 
 
-    auto Model=ModelID::getModel(argv[1]);
-    if(Model==ModelID::ModelIDs::NotSet) {
+    ReturnType res;
+    std::string prefix{"--"};
+    bool UsePrefix = StringStartsWith(args.at(0),prefix);
+    if(UsePrefix)
+    {
+        for(const auto& arg: args)
+        {
+            auto el = arg;
+            std::transform(el.begin(), el.end(), el.begin(), ::tolower);
+            if(StringStartsWith(el,"--model="))
+            {
+                res.Model = BSMPT::ModelID::getModel(el.substr(std::string("--model=").size()));
+            }
+            else if(StringStartsWith(el,"--input="))
+            {
+                res.InputFile = arg.substr(std::string("--input=").size());
+            }
+            else if(StringStartsWith(el,"--output="))
+            {
+                res.OutputFile = arg.substr(std::string("--output=").size());
+            }
+            else if(StringStartsWith(el,"--line="))
+            {
+                res.Line = std::stoi(el.substr(std::string("--line=").size()));
+            }
+            else if(StringStartsWith(el,"--numberofsteps="))
+            {
+                res.NumberOfSteps = std::stoi(el.substr(std::string("--numberofsteps=").size()));
+            }
+            else if(StringStartsWith(el,"--terminaloutput="))
+            {
+                res.TerminalOutput = el.substr(std::string("--lastline=").size()) == "y";
+            }
+            else if(StringStartsWith(el,"--vw="))
+            {
+                res.vw = std::stod(el.substr(std::string("--vw=").size()));
+            }
+            else if(StringStartsWith(el,"--config="))
+            {
+                res.ConfigFile = arg.substr(std::string("--config").size());
+            }
+        }
+    }
+    else{
+        res.Model = ModelID::getModel(args.at(0));
+        res.InputFile = args.at(1);
+        res.OutputFile = args.at(2);
+        res.Line = std::stoi(args.at(3));
+        res.NumberOfSteps = std::stoi(args.at(4));
+        res.ConfigFile = args.at(5);
+        if(argc == 8) {
+            std::string s7 = argv[6];
+            res.TerminalOutput = ("y" == s7);
+        }
+    }
+
+    if(res.NumberOfSteps == 0)
+    {
+        throw std::runtime_error("You have set the number of steps to zero.");
+    }
+
+
+    return res;
+}
+
+
+
+
+int main(int argc, char *argv[]) try{
+
+    const auto args = getCLIArguments(argc,argv);
+
+    if(args.Model==ModelID::ModelIDs::NotSet) {
         std::cerr << "Your Model parameter does not match with the implemented Models." << std::endl;
         ShowInputError();
         return EXIT_FAILURE;
     }
 
-    double LineNumber,NumberOfStep;
-    char* in_file;char* out_file;
-
-    in_file = argv[2];
-    out_file = argv[3];
-
-    LineNumber = atoi(argv[4]);
-    NumberOfStep = atoi(argv[5]);
-
-
-    bool TerminalOutput = false;
-    if(argc == 8) {
-        std::string s7 = argv[7];
-        std::cout << s7 << std::endl;
-        TerminalOutput = ("y" == s7);
-        if(s7!="y") throw std::runtime_error("To many arguments? Do you want terminal output?");
-    }
     //Init: Interface Class for the different transport methods
     Baryo::CalculateEtaInterface EtaInterface(argv[6] /* = Config file */);
-    if(LineNumber < 1)
+    if(args.Line < 1)
     {
         std::cout << "Start line counting with 1" << std::endl;
         return EXIT_FAILURE;
     }
     int linecounter = 1;
-    std::ifstream infile(in_file);
+    std::ifstream infile(args.InputFile);
     if(!infile.good()) {
         std::cout << "Input file not found " << std::endl;
         return EXIT_FAILURE;
     }
-    std::ofstream outfile(out_file);
+    std::ofstream outfile(args.OutputFile);
     if(!outfile.good())
     {
-        std::cout << "Can not create file " << out_file << std::endl;
+        std::cout << "Can not create file " << args.OutputFile << std::endl;
         return EXIT_FAILURE;
     }
     std::string linestr;
-    std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer = ModelID::FChoose(Model);
+    std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer = ModelID::FChoose(args.Model);
     std::vector<std::string> etaLegend = EtaInterface.legend();// Declare the vector for the PTFinder algorithm
     size_t nPar,nParCT;
     nPar = modelPointer->get_nPar();
@@ -113,7 +202,7 @@ int main(int argc, char *argv[]) try{
 
     while(getline(infile,linestr))
     {
-        if(linecounter > LineNumber) break;
+        if(linecounter > args.Line) break;
 
         if(linecounter == 1)
           {
@@ -134,40 +223,41 @@ int main(int argc, char *argv[]) try{
 
             modelPointer->setUseIndexCol(linestr);
           }
-        if(LineNumber==linecounter)
+        if(args.Line==linecounter)
         {
             std::pair<std::vector<double>,std::vector<double>> parameters = modelPointer->initModel(linestr);
             par=parameters.first;
             parCT = parameters.second;
-            if(TerminalOutput) modelPointer->write();
+
+            if(args.TerminalOutput) modelPointer->write();
 
 
-            if(TerminalOutput)
+            if(args.TerminalOutput)
             {
                 std::cout<<"Calculating EWPT in default settings with:\n mu = "<<modelPointer->get_scale()
                         <<std::endl;
             }
-            if(TerminalOutput)std::cout<<"Start of mu variation"<<std::endl;
-            double nstep = NumberOfStep;
-            for(int step=0;step<nstep;step++){
-                double mu_factor = 1/2. + (step/nstep);
-                if(TerminalOutput) std::cout<<"\r currently mu_factor = "<<mu_factor<<std::endl;
+            if(args.TerminalOutput)std::cout<<"Start of mu variation"<<std::endl;
+            for(int step=0;step<args.NumberOfSteps;step++){
+                double mu_factor = 1/2. + (step/static_cast<double>(args.NumberOfSteps));
+                if(args.TerminalOutput) std::cout<<"\r currently mu_factor = "<<mu_factor<<std::endl;
+
                 auto VEVnames = modelPointer->addLegendTemp();
                 auto CT_mu=modelPointer->resetScale(C_vev0*mu_factor);
                 auto EWPT_mu = Minimizer::PTFinder_gen_all(modelPointer,0,300);
                 std::vector<double> startpoint;
-                for(auto x : EWPT_mu.EWMinimum) startpoint.push_back(x/2.);
+                for(const auto& x : EWPT_mu.EWMinimum) startpoint.push_back(x/2.);
                 if(EWPT_mu.StatusFlag==1){
                     std::vector<double>checkmu;
                     auto VEV_mu_sym = Minimizer::Minimize_gen_all(modelPointer,EWPT_mu.Tc+1,checkmu,startpoint);
                     auto VEV_mu_brk = EWPT_mu.EWMinimum;
-                    auto eta_mu = EtaInterface.CalcEta(vw,EWPT_mu.EWMinimum,VEV_mu_sym,EWPT_mu.Tc,modelPointer);
+                    auto eta_mu = EtaInterface.CalcEta(args.vw,EWPT_mu.EWMinimum,VEV_mu_sym,EWPT_mu.Tc,modelPointer);
 
                     outfile << linestr;
                     outfile << sep << mu_factor <<sep<< mu_factor*C_vev0;
                     outfile << sep << EWPT_mu.Tc<<sep<<EWPT_mu.vc<<sep<<EWPT_mu.vc/EWPT_mu.Tc<<sep<<EWPT_mu.EWMinimum;
                     outfile << sep << EWPT_mu.StatusFlag;
-                    outfile << sep << vw;
+                    outfile << sep << args.vw;
                     outfile << sep << EtaInterface.getLW();
                     outfile << sep << EtaInterface.GSL_integration_mubl_container.getSymmetricCPViolatingPhase_top();
                     outfile << sep << EtaInterface.GSL_integration_mubl_container.getBrokenCPViolatingPhase_top();
@@ -182,7 +272,7 @@ int main(int argc, char *argv[]) try{
 
                 }//EWPT@mu found
                 else {
-                    if(TerminalOutput) std::cout<<"\tNo SFOEWPT found for given scale"<<std::endl;
+                    if(args.TerminalOutput) std::cout<<"\tNo SFOEWPT found for given scale"<<std::endl;
                     continue;
                 }
         }//END: Mu Factor
@@ -190,13 +280,16 @@ int main(int argc, char *argv[]) try{
         linecounter++;
         if(infile.eof()) break;
     }
-    if(TerminalOutput) std::cout << std::endl;
+    if(args.TerminalOutput) std::cout << std::endl;
     outfile.close();
 
     return EXIT_SUCCESS;
 
 }
-
+catch(int)
+{
+    return EXIT_SUCCESS;
+}
 catch(exception& e){
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
