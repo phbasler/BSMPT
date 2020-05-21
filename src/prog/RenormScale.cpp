2936628af255097239993c24fa0b1dee538c7513
Philipp Basler <philipp.basler@kit.edu>
2020-05-21 13:41:44 +0200

    Added CLI options for RenormScale

--- src/prog/RenormScale.cpp
+++ src/prog/RenormScale.cpp
@@ -33,10 +33,24 @@
 #include <BSMPT/minimizer/Minimizer.h>
 #include <BSMPT/utility.h>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 
 using namespace std;
 using namespace BSMPT;
+
+struct CLIoptions{
+    BSMPT::ModelID::ModelIDs Model{ModelID::ModelIDs::NotSet};
+    int Line{};
+    std::string InputFile, OutputFile;
+    int NumberOfSteps{};
+    bool TerminalOutput{false};
+    bool UseGSL { Minimizer::UseGSLDefault};
+    bool UseCMAES {Minimizer::UseLibCMAESDefault};
+    bool UseNLopt{Minimizer::UseNLoptDefault};
+    int WhichMinimizer{Minimizer::WhichMinimizerDefault};
+};
+CLIoptions getCLIArguments(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) try{
     /**
@@ -45,57 +59,33 @@ int main(int argc, char *argv[]) try{
      */
     bool PrintErrorLines=true;
 
-    if(!( argc == 6 or argc == 7) )
-    {
-        std::cerr << "./RenormScale Model Inputfile Outputfile  Line NumberOfSteps TerminalOutput(y)\n";
-        ShowInputError();
-        return EXIT_FAILURE;
-    }
+    const auto args = getCLIArguments(argc,argv);
 
-
-    auto Model=ModelID::getModel(argv[1]);
-    if(Model==ModelID::ModelIDs::NotSet) {
+    if(args.Model==ModelID::ModelIDs::NotSet) {
         std::cerr << "Your Model parameter does not match with the implemented Models." << std::endl;
         ShowInputError();
         return EXIT_FAILURE;
     }
 
-    double LineNumber,NumberOfStep;
-    char* in_file;char* out_file;
-
-    in_file = argv[2];
-    out_file = argv[3];
-
-    LineNumber = atoi(argv[4]);
-    NumberOfStep = atoi(argv[5]);
-
-
-    bool TerminalOutput = false;
-    if(argc == 7) {
-        std::string s7 = argv[6];
-        std::cout << s7 << std::endl;
-        TerminalOutput = ("y" == s7);
-        if(s7!="y") throw std::runtime_error("To many arguments? Do you want terminal output?");
-    }
-    if(LineNumber < 1)
+    if(args.Line < 1)
     {
         std::cout << "Start line counting with 1" << std::endl;
         return EXIT_FAILURE;
     }
     int linecounter = 1;
-    std::ifstream infile(in_file);
+    std::ifstream infile(args.InputFile);
     if(!infile.good()) {
         std::cout << "Input file not found " << std::endl;
         return EXIT_FAILURE;
     }
-    std::ofstream outfile(out_file);
+    std::ofstream outfile(args.OutputFile);
     if(!outfile.good())
     {
-        std::cout << "Can not create file " << out_file << std::endl;
+        std::cout << "Can not create file " << args.OutputFile << std::endl;
         return EXIT_FAILURE;
     }
     std::string linestr;
-    std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer = ModelID::FChoose(Model);
+    std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer = ModelID::FChoose(args.Model);
     std::size_t nPar,nParCT;
     nPar = modelPointer->get_nPar();
     nParCT = modelPointer->get_nParCT();
@@ -104,7 +94,7 @@ int main(int argc, char *argv[]) try{
 
     while(getline(infile,linestr))
     {
-        if(linecounter > LineNumber) break;
+        if(linecounter > args.Line) break;
 
         if(linecounter == 1)
           {
@@ -113,31 +103,30 @@ int main(int argc, char *argv[]) try{
 
             modelPointer->setUseIndexCol(linestr);
           }
-        if(LineNumber==linecounter)
+        if(args.Line==linecounter)
         {
-            std::pair<std::vector<double>,std::vector<double>> parameters = modelPointer->initModel(linestr);
+            auto parameters = modelPointer->initModel(linestr);
             par=parameters.first;
             parCT = parameters.second;
-            if(TerminalOutput) modelPointer->write();
+            if(args.TerminalOutput) modelPointer->write();
 
-            if(TerminalOutput){
+            if(args.TerminalOutput){
                 std::cout<<"Calculating EWPT in default settings with:\n mu = "
                                       <<modelPointer->get_scale()<<std::endl;
             }
-            auto EWPTdefault = Minimizer::PTFinder_gen_all(modelPointer,0,300);
+            auto EWPTdefault = Minimizer::PTFinder_gen_all(modelPointer,0,300,args.WhichMinimizer);
             std::vector<double> vevsymmetricSolution,checksym, startpoint;
             for(std::size_t i=0;i<modelPointer->get_nVEV();i++) startpoint.push_back(0.5*EWPTdefault.EWMinimum.at(i));
             auto VEVsym = Minimizer::Minimize_gen_all(modelPointer,EWPTdefault.Tc+1,checksym,startpoint);
-            if(TerminalOutput)std::cout<<"Start of mu variation"<<std::endl;
-            double nstep = NumberOfStep;
-            for(int step=0;step<nstep;step++){
-                double mu_factor = 1/2. + (step/nstep);
+            if(args.TerminalOutput)std::cout<<"Start of mu variation"<<std::endl;
+            for(int step=0;step<args.NumberOfSteps;step++){
+                double mu_factor = 1/2. + (step/static_cast<double>(args.NumberOfSteps));
                 auto VEVnames = modelPointer->addLegendTemp();
                 auto CT_mu=modelPointer->resetScale(C_vev0*mu_factor);
-                auto EWPT_mu = Minimizer::PTFinder_gen_all(modelPointer,0,300);
+                auto EWPT_mu = Minimizer::PTFinder_gen_all(modelPointer,0,300,args.WhichMinimizer);
                 std::vector<double>checkmu;
-                auto VEV_NLO_mu = Minimizer::Minimize_gen_all(modelPointer,0,checkmu,startpoint);
-                if(TerminalOutput){
+                auto VEV_NLO_mu = Minimizer::Minimize_gen_all(modelPointer,0,checkmu,startpoint,args.WhichMinimizer);
+                if(args.TerminalOutput){
                     std::cout<<"\tStatusFlag = "<<static_cast<int>(EWPT_mu.StatusFlag)
                             <<" @ mu = "<<modelPointer->get_scale()<<" = "<<mu_factor<<"*C_vev0"<<std::endl;
                     for(std::size_t i=0;i<EWPT_mu.EWMinimum.size();i++)
@@ -175,14 +164,152 @@ int main(int argc, char *argv[]) try{
         linecounter++;
         if(infile.eof()) break;
     }
-    if(TerminalOutput) std::cout << std::endl;
+    if(args.TerminalOutput) std::cout << std::endl;
     outfile.close();
 
     return EXIT_SUCCESS;
 
 }
-
+catch(int)
+{
+    return EXIT_SUCCESS;
+}
 catch(exception& e){
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
+}
+
+
+CLIoptions getCLIArguments(int argc, char *argv[])
+{
+
+
+    std::vector<std::string> args;
+    for(int i{1};i<argc;++i) args.push_back(argv[i]);
+
+    if(argc < 6 or args.at(0) == "--help")
+    {
+        int SizeOfFirstColumn = std::string("--TerminalOutput=           ").size();
+        std::cout << std::boolalpha
+                  << "RenormScale varies the MSBar scale and calculates the EWPT at each scale between 0.5 and 1.5 times the set scale of the model" << std::endl
+                  << "It is called either by " << std::endl
+                  << "./RenormScale Model Inputfile Outputfile  Line NumberOfSteps TerminalOutput(y)" << std::endl
+                  << "or with the following arguments" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<< "--help"
+                  << "Shows this menu" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left << "--model="
+                  << "The model you want to investigate"<<std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--input="
+                  << "The input file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--output="
+                  << "The output file in tsv format" << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--Line="
+                  <<"The parameter point to investigate. Expects line 1 to be a legend." << std::endl
+                  << std::setw(SizeOfFirstColumn) << std::left<<"--NumberOfSteps="
+                  <<"Number of steps between 0.5 and 1.5 times scale." << std::endl;
+        std::string GSLhelp{"--UseGSL="};
+        GSLhelp += Minimizer::UseGSLDefault?"true":"false";
+        std::cout << std::setw(SizeOfFirstColumn) << std::left <<GSLhelp
+                  << "Use the GSL library to minimize the effective potential" << std::endl;
+        std::string CMAEShelp{"--UseCMAES="};
+        CMAEShelp += Minimizer::UseLibCMAESDefault?"true":"false";
+        std::cout << std::setw(SizeOfFirstColumn) << std::left <<CMAEShelp
+                  << "Use the CMAES library to minimize the effective potential" << std::endl;
+        std::string NLoptHelp{"--UseNLopt="};
+        NLoptHelp += Minimizer::UseNLoptDefault?"true":"false";
+        std::cout << std::setw(SizeOfFirstColumn) << std::left <<NLoptHelp
+                  << "Use the NLopt library to minimize the effective potential" << std::endl;
+        std::cout<< std::setw(SizeOfFirstColumn) << std::left<<"--TerminalOutput="
+                  <<"y/n Turns on additional information in the terminal during the calculation." << std::endl;
+        ShowInputError();
+    }
+
+    if(args.size() > 0 and args.at(0)=="--help")
+    {
+        throw int{0};
+    }
+    else if(argc < 6)
+    {
+        throw std::runtime_error("Too few arguments.");
+    }
+
+
+    CLIoptions res;
+    std::string prefix{"--"};
+    bool UsePrefix = StringStartsWith(args.at(0),prefix);
+    if(UsePrefix)
+    {
+        for(const auto& arg: args)
+        {
+            auto el = arg;
+            std::transform(el.begin(), el.end(), el.begin(), ::tolower);
+            if(StringStartsWith(el,"--model="))
+            {
+                res.Model = BSMPT::ModelID::getModel(el.substr(std::string("--model=").size()));
+            }
+            else if(StringStartsWith(el,"--input="))
+            {
+                res.InputFile = arg.substr(std::string("--input=").size());
+            }
+            else if(StringStartsWith(el,"--output="))
+            {
+                res.OutputFile = arg.substr(std::string("--output=").size());
+            }
+            else if(StringStartsWith(el,"--line="))
+            {
+                res.Line = std::stoi(el.substr(std::string("--line=").size()));
+            }
+            else if(StringStartsWith(el,"--numberofsteps="))
+            {
+                res.NumberOfSteps = std::stoi(el.substr(std::string("--numberofsteps=").size()));
+            }
+            else if(StringStartsWith(el,"--terminaloutput="))
+            {
+                res.TerminalOutput = el.substr(std::string("--terminaloutput=").size()) == "y";
+            }
+            else if(StringStartsWith(el,"--usegsl="))
+            {
+                res.UseGSL = arg.substr(std::string("--usegsl=").size()) == "true";
+                if(res.UseGSL and not Minimizer::UseGSLDefault)
+                {
+                    throw std::runtime_error("You set --UseGSL=true but GSL was not found during compilation.");
+                }
+            }
+            else if(StringStartsWith(el,"--usecmaes="))
+            {
+                res.UseCMAES = arg.substr(std::string("--usecmaes=").size()) == "true";
+                if(res.UseCMAES and not Minimizer::UseLibCMAESDefault)
+                {
+                    throw std::runtime_error("You set --UseCMAES=true but CMAES was not found during compilation.");
+                }
+            }
+            else if(StringStartsWith(el,"--usenlopt="))
+            {
+                res.UseNLopt = arg.substr(std::string("--usenlopt=").size()) == "true";
+                if(res.UseNLopt and not Minimizer::UseNLoptDefault)
+                {
+                    throw std::runtime_error("You set --UseNLopt=true but NLopt was not found during compilation.");
+                }
+            }
+        }
+        res.WhichMinimizer = Minimizer::CalcWhichMinimizer(res.UseGSL,res.UseCMAES,res.UseNLopt);
+    }
+    else{
+        res.Model = ModelID::getModel(args.at(0));
+        res.InputFile = args.at(1);
+        res.OutputFile = args.at(2);
+        res.Line = std::stoi(args.at(3));
+        res.NumberOfSteps = std::stoi(args.at(4));
+        if(argc == 7) {
+            res.TerminalOutput = ("y" == std::string(argv[5]));
+        }
+    }
+
+    if(res.WhichMinimizer == 0)
+    {
+        throw std::runtime_error("You disabled all minimizers. You need at least one.");
+    }
+
+
+    return res;
 }
