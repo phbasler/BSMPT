68b9ccd5e085f4b21b3b13160a27cf795263a756
Philipp Basler <philipp.basler@kit.edu>
2021-04-13 23:58:41 +0200

    Triple Higgs couplings added to unit tests

---
+++ tests/GenerateTestCompares/C2HDM.h
@@ -0,0 +1,15 @@
+#include <BSMPT/minimizer/Minimizer.h>
+#include <map>
+#include <vector>
+class Compare_C2HDM
+{
+public:
+  using Matrix3D = std::vector<std::vector<std::vector<double>>>;
+  using Matrix2D = std::vector<std::vector<double>>;
+  Compare_C2HDM();
+  Matrix3D CheckTripleCT;
+  Matrix3D CheckTripleCW;
+  Matrix3D CheckTripleTree;
+
+  std::map<int, BSMPT::Minimizer::EWPTReturnType> EWPTPerSetting;
+};
--- tests/Test-c2hdm.cpp
+++ tests/Test-c2hdm.cpp
@@ -5,6 +5,9 @@
 #include <BSMPT/models/IncludeAllModels.h>
 #include <BSMPT/models/ModelTestfunctions.h>
 
+#include "GenerateTestCompares/C2HDM.h"
+#include <fstream>
+
 const std::vector<double> example_point_C2HDM{/* lambda_1 = */ 3.29771,
                                               /* lambda_2 = */ 0.274365,
                                               /* lambda_3 = */ 4.71019,
@@ -14,6 +17,8 @@ const std::vector<double> example_point_C2HDM{/* lambda_1 = */ 3.29771,
                                               /* Re(m_{12}^2) = */ 2706.86,
                                               /* tan(beta) = */ 4.64487,
                                               /* Yukawa Type = */ 1};
+
+const Compare_C2HDM Expected;
 
 TEST_CASE("Checking NLOVEV for C2HDM", "[c2hdm]")
 {
@@ -35,7 +40,7 @@ TEST_CASE("Checking NLOVEV for C2HDM", "[c2hdm]")
   }
 }
 
-TEST_CASE("Checking EWPT for C2HDM", "[c2hdm]")
+TEST_CASE("Checking EWPT for C2HDM", "[c2hdm-ewpt]")
 {
   using namespace BSMPT;
   std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer =
@@ -44,10 +49,12 @@ TEST_CASE("Checking EWPT for C2HDM", "[c2hdm]")
   std::vector<double> Check;
   auto EWPT = Minimizer::PTFinder_gen_all(
       modelPointer, 0, 300, Minimizer::WhichMinimizerDefault);
-  const double omega_c_expected = 200.79640966130026;
-  const double Tc_expected      = 145.56884765625;
-  const std::vector<double> min_expected{
-      0, -49.929666284908336, -194.48507400070247, 1.3351211509361505};
+  const double omega_c_expected =
+      Expected.EWPTPerSetting.at(Minimizer::WhichMinimizerDefault).vc;
+  const double Tc_expected =
+      Expected.EWPTPerSetting.at(Minimizer::WhichMinimizerDefault).Tc;
+  const std::vector<double> min_expected =
+      Expected.EWPTPerSetting.at(Minimizer::WhichMinimizerDefault).EWMinimum;
   REQUIRE(EWPT.StatusFlag == Minimizer::MinimizerStatus::SUCCESS);
   REQUIRE(std::abs(omega_c_expected - EWPT.vc) / omega_c_expected <= 1e-4);
   REQUIRE(std::abs(Tc_expected - EWPT.Tc) / Tc_expected <= 1e-4);
@@ -226,3 +233,86 @@ TEST_CASE("Checking the identities required to vanish for the CT in the C2HDM",
   auto result = ModelTests::CheckCTIdentities(*modelPointer);
   REQUIRE(result == ModelTests::TestResults::Pass);
 }
+
+TEST_CASE("Checking triple higgs NLO couplings in the C2HDM", "[c2hdm]")
+{
+
+  using namespace BSMPT;
+  std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer =
+      ModelID::FChoose(ModelID::ModelIDs::C2HDM);
+  modelPointer->initModel(example_point_C2HDM);
+  modelPointer->Prepare_Triple();
+  modelPointer->TripleHiggsCouplings();
+
+  auto Check = [](auto result, auto expected) {
+    if (expected != 0)
+    {
+      REQUIRE(std::abs(result - expected) / std::abs(expected) < 1e-4);
+    }
+    else
+    {
+      REQUIRE(std::abs(result) < 1e-4);
+    }
+  };
+
+  auto NHiggs = modelPointer->get_NHiggs();
+  for (std::size_t i{0}; i < NHiggs; ++i)
+  {
+    for (std::size_t j{0}; j < NHiggs; ++j)
+    {
+      for (std::size_t k{0}; k < NHiggs; ++k)
+      {
+        Check(modelPointer->get_TripleHiggsCorrectionsTreePhysical(i, j, k),
+              Expected.CheckTripleTree.at(i).at(j).at(k));
+        Check(modelPointer->get_TripleHiggsCorrectionsCTPhysical(i, j, k),
+              Expected.CheckTripleCT.at(i).at(j).at(k));
+        Check(modelPointer->get_TripleHiggsCorrectionsCWPhysical(i, j, k),
+              Expected.CheckTripleCW.at(i).at(j).at(k));
+      }
+    }
+  }
+}
+
+// TEST_CASE("Create EWPT compares", "[c2hdm-setup]")
+//{
+//  using namespace BSMPT;
+//  std::shared_ptr<BSMPT::Class_Potential_Origin> modelPointer =
+//      ModelID::FChoose(ModelID::ModelIDs::C2HDM);
+//  modelPointer->initModel(example_point_C2HDM);
+
+//  int WhichMin;
+//  Minimizer::EWPTReturnType EWPT;
+
+//  std::ofstream outfile("dummy.txt");
+
+//  std::map<int, Minimizer::EWPTReturnType> mdata;
+//  for (bool UseGSL : {false, true})
+//  {
+//    for (bool UseCMAES : {false, true})
+//    {
+//      for (bool UseNLopt : {false, true})
+//      {
+//        if (not UseGSL and not UseCMAES and not UseNLopt) continue;
+
+//        WhichMin = Minimizer::CalcWhichMinimizer(UseGSL, UseCMAES, UseNLopt);
+//        EWPT     = Minimizer::PTFinder_gen_all(modelPointer, 0, 300,
+//        WhichMin); mdata[WhichMin] = EWPT; outfile << "EWPTPerSetting[" <<
+//        WhichMin
+//                << "].Tc = " << mdata[WhichMin].Tc << ";" << std::endl
+//                << "EWPTPerSetting[" << WhichMin
+//                << "].vc = " << mdata[WhichMin].vc << ";" << std::endl;
+//        for (const auto &el : EWPT.EWMinimum)
+//        {
+//          if (std::abs(el) > 1e-5)
+//            outfile << "EWPTPerSetting[" << WhichMin <<
+//            "].EWMinimum.push_back("
+//                    << el << ");" << std::endl;
+//          else
+//            outfile << "EWPTPerSetting[" << WhichMin <<
+//            "].EWMinimum.push_back("
+//                    << 0 << ");" << std::endl;
+//        }
+//      }
+//    }
+//  }
+//}
