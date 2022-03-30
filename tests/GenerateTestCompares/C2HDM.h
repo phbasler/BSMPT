df7d45265550bbd63113b8eb7b54a4827551008e
Philipp Basler <philipp.basler@kit.edu>
2022-03-30 18:51:59 +0200

    Added missing authors

--- tests/GenerateTestCompares/C2HDM.h
+++ tests/GenerateTestCompares/C2HDM.h
@@ -1,4 +1,5 @@
-// SPDX-FileCopyrightText: 2021 Philipp Basler
+// SPDX-FileCopyrightText: 2021 Philipp Basler, Margarete Mühlleitner and Jonas
+// Müller
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include <BSMPT/minimizer/Minimizer.h>
