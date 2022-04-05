df154560e966b9e4dc454b26ab033f17b9751e3a
github-action-benchmark <github@users.noreply.github.com>
2022-04-05 23:43:57 +0000

    add C++ Benchmark (googlecpp) benchmark result for b96159bb044330d3af867e00714cdca51bfa3ca9

---
+++ benchmarks/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1649202237564,
+  "repoUrl": "https://github.com/phbasler/BSMPT",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "philipbasler@gmail.com",
+            "name": "Phil"
+          },
+          "committer": {
+            "email": "philipbasler@gmail.com",
+            "name": "Phil"
+          },
+          "distinct": true,
+          "id": "b96159bb044330d3af867e00714cdca51bfa3ca9",
+          "message": "Doxygen deploys to documentation and benchmarks to benchmark",
+          "timestamp": "2022-04-06T01:37:14+02:00",
+          "tree_id": "f82b10f928853f9b6cdcc633f105e28d5ebbe567",
+          "url": "https://github.com/phbasler/BSMPT/commit/b96159bb044330d3af867e00714cdca51bfa3ca9"
+        },
+        "date": 1649202234863,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_NLOVEV",
+            "value": 365702041.39999306,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 447739.99999999645 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_EWPT",
+            "value": 5852521580.00003,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 7572600.00000004 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
