3b4012f771dd61816362f2a665b687eff3b0e17e
Philipp Basler <philipp.basler@kit.edu>
2022-10-14 21:47:28 +0200

    apply cmake-format

--- tools/cmake/ConanDependencies.cmake
+++ tools/cmake/ConanDependencies.cmake
@@ -2,6 +2,10 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
 include(conan)
 set(ConanPkgList eigen/3.4.0 gsl/2.7 boost/1.78.0 nlohmann_json/3.10.5)
 
+if(BSMPT_IS_TOPLEVEL AND BUILD_TESTING)
+  set(ConanPkgList ${ConanPkgList} catch2/3.1.0)
+endif()
+
 if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
   set(ConanPkgList ${ConanPkgList} benchmark/1.6.1)
 endif()
