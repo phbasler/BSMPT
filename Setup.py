a5764047c2e77fd1e47eef54dad4f5ac6435021d
Philipp Basler <philipp.basler@kit.edu>
2024-04-07 17:08:03 +0200

    add options to conan file

--- Setup.py
+++ Setup.py
@@ -3,31 +3,30 @@ import sys
 import os
 import shutil
 
-build_profiles = {
-    "win32": "windows-release",
-    "linux": "linux-release"
-}
+build_profiles = {"win32": "windows-release", "linux": "linux-release"}
 
 target_profiles = {
-    "win32": [ "windows-release", "windows-debug"],
-    "linux": ["linux-release", "linux-debug"]
+    "win32": ["windows-release", "windows-debug"],
+    "linux": ["linux-release", "linux-debug"],
 }
 
 
 def setup_profiles():
-    conan_home=subprocess.check_output('conan config home'.split(),encoding='UTF-8')
-    profile_dir=os.path.join(str(conan_home.split()[0]),"profiles","BSMPT")
+    conan_home = subprocess.check_output("conan config home".split(), encoding="UTF-8")
+    profile_dir = os.path.join(str(conan_home.split()[0]), "profiles", "BSMPT")
     print(profile_dir)
     try:
-        shutil.copytree("profiles/BSMPT",profile_dir)
+        shutil.copytree("profiles/BSMPT", profile_dir)
     except:
         pass
 
 
-def conan_install(profile, additional_options = []):
-    config_settings = ["tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type']"]
+def conan_install(profile, additional_options=[]):
+    config_settings = [
+        "tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type']"
+    ]
 
-    build_profile=build_profiles[sys.platform]
+    build_profile = build_profiles[sys.platform]
 
     cmd = f"conan install . -pr:h BSMPT/{profile} -pr:b BSMPT/{build_profile} ".split()
 
@@ -39,9 +38,11 @@ def conan_install(profile, additional_options = []):
 
     subprocess.check_call(cmd)
 
+
 def conan_install_all():
     for profile in target_profiles[sys.platform]:
         conan_install(profile)
+
 
 if __name__ == "__main__":
     setup_profiles()
--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,28 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeToolchain
+
 
 class BSMPT(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
-    options = { "enable_tests": [True, False]}
-    default_options = {"enable_tests": True}
-    
+    options = {
+        "enable_tests": [True, False],
+        "UseLibCMAES": [True, False],
+        "UseNLopt": [True, False],
+        "MakeAdditionalTesting": [True, False],
+        "BSMPTCompileBaryo": [True, False],
+        "EnableCoverage": [True, False],
+    }
+    default_options = {
+        "enable_tests": True,
+        "UseLibCMAES": True,
+        "UseNLopt": True,
+        "MakeAdditionalTesting": False,
+        "BSMPTCompileBaryo": True,
+        "EnableCoverage": True,
+    }
+
     def requirements(self):
         self.requires("eigen/3.4.0")
         self.requires("boost/1.84.0")
@@ -18,6 +33,19 @@ class BSMPT(ConanFile):
     def build_requirements(self):
         self.tool_requires("cmake/3.29.0")
         self.test_requires("catch2/3.5.3")
+        self.test_requires("benchmark/1.6.1")
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain()
+
+        tc.variables["BUILD_TESTING"] = self.options.enable_tests
+        tc.variables["UseLibCMAES"] = self.options.UseLibCMAES
+        tc.variables["UseNLopt"] = self.options.UseNLopt
+        tc.variables["MakeAdditionalTesting"] = self.options.MakeAdditionalTesting
+        tc.variables["BSMPTCompileBaryo"] = self.options.BSMPTCompileBaryo
+        tc.variables["EnableCoverage"] = self.options.EnableCoverage
+
+        tc.generate()
