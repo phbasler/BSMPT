3e0eb4c770ca360bf7c964a90878084a7c8181f5
João Viana <57032457+vollous@users.noreply.github.com>
2024-05-13 11:03:41 +0100

    Fix recursive step that assumed l0 = 0.

--- src/bounce_solution/action_calculation.cpp
+++ src/bounce_solution/action_calculation.cpp
@@ -474,7 +474,7 @@ std::vector<double> BounceActionInt::ExactSolutionLin(double l0,
       ss << rho_up << "\t" << l << "\t" << dVdl << "\t" << d2Vdl2 << "\n";
       BSMPT::Logger::Write(BSMPT::LoggingLevel::BounceDetailed, ss.str());
       ss.str(std::string());
-      return (ExactSolutionLin(l0, l / 10., dVdl, d2Vdl2));
+      return (ExactSolutionLin(l0, l0 + (l - l0) / 10., dVdl, d2Vdl2));
     }
   }
   else
