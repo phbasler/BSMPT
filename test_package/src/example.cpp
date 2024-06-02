e52b60994919259eacd9faec595f04b74f147357
Philipp Basler <philipp.basler@kit.edu>
2024-06-02 22:55:50 +0200

    Call model

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -2,13 +2,16 @@
 #include <BSMPT/utility/asciiplotter/asciiplotter.h>
 #include <BSMPT/utility/spline/spline.h>
 #include <BSMPT/bounce_solution/action_calculation.h>
-/*#include <BSMPT/gravitational_waves/gw.h>
+#include <BSMPT/gravitational_waves/gw.h>
 #include <BSMPT/minimizer/Minimizer.h>
 #include <BSMPT/minimum_tracer/minimum_tracer.h>
 #include <BSMPT/models/IncludeAllModels.h>
 #include <BSMPT/ThermalFunctions/ThermalFunctions.h>
-#include <BSMPT/transition_tracer/transition_tracer.h>*/
+#include <BSMPT/transition_tracer/transition_tracer.h>
 
 int main() {
-    BSMPT::Logger::Disable();
+    using namespace BSMPT;
+    Logger::Disable();
+    ModelID::FChoose(ModelID::ModelIDs::C2HDM, GetSMConstants());
+
 }
