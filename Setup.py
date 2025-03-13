58a451cfec146f1bd8f1aa9592df5aa2e7af2371
Philipp Basler <28863303+phbasler@users.noreply.github.com>
2025-03-13 07:26:04 +0100

    Dont include compiler in git profile name

--- Setup.py
+++ Setup.py
@@ -156,8 +156,13 @@ def conan_install(
     profile, additional_options=[], build_missing=False, compiler: Compiler = None
 ):
     config_settings = [
-        "tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type','settings.compiler']"
+        "tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type']"
     ]
+
+    if 'GITHUB_ENV' not in os.environ:
+        config_settings = [
+            "tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type','settings.compiler']"
+        ]
 
     build_profile = get_profile(sys.platform, get_arch(), BuildMode.release, compiler)
 
