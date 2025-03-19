b9854a5e28c851671f99ce60fdf6824c6c55cba3
João Viana <57032457+vollous@users.noreply.github.com>
2025-03-19 18:32:42 +0000

    Missing T^3 and sqrt in the pressure at NLO for the $\gamma$ scaling.

--- src/gravitational_waves/gw.cpp
+++ src/gravitational_waves/gw.cpp
@@ -649,13 +649,15 @@ double Getkappa_col(const double &Tstar,
   if (pnlo_scaling == 1)
   {
     // Mass matrix is ordered
-    P_NLO += GaugeSq_True.at(0) * A_coupling;
-    P_NLO += 2 * GaugeSq_True.at(1) * W_coupling;
-    P_NLO += GaugeSq_True.at(3) * Z_coupling;
+    P_NLO += sqrt(GaugeSq_True.at(0)) * A_coupling;
+    P_NLO += 2 * sqrt(GaugeSq_True.at(1)) * W_coupling;
+    P_NLO += sqrt(GaugeSq_True.at(3)) * Z_coupling;
 
-    P_NLO -= GaugeSq_False.at(0) * A_coupling;
-    P_NLO -= 2 * GaugeSq_False.at(1) * W_coupling;
-    P_NLO -= GaugeSq_False.at(3) * Z_coupling;
+    P_NLO -= sqrt(GaugeSq_False.at(0)) * A_coupling;
+    P_NLO -= 2 * sqrt(GaugeSq_False.at(1)) * W_coupling;
+    P_NLO -= sqrt(GaugeSq_False.at(3)) * Z_coupling;
+
+    P_NLO *= pow(Tstar, 3);
   }
   else if (pnlo_scaling == 2)
   {
