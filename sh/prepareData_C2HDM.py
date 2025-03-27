52bef278908ced948df2354e1b407da156dfc50e
Philipp Basler <28863303+phbasler@users.noreply.github.com>
2025-03-27 20:44:18 +0100

    Apply black

--- sh/prepareData_C2HDM.py
+++ sh/prepareData_C2HDM.py
@@ -7,45 +7,59 @@
 import pandas as pd
 import argparse
 
-'''
+"""
 The parameters Type,Lambda1 to Lambda4, re_Lambda5, im_Lambda5, tanbeta,
 re_m12squared should have the label of the corresponding parameter.
-'''
+"""
 
-Type = 'yuktype'
-Lambda1 = 'L1'
-Lambda2 = 'L2'
-Lambda3 = 'L3'
-Lambda4 = 'L4'
-re_Lambda5 = 'L5r'
-im_Lambda5 = 'L5i'
-tanbeta = 'tbeta'
-re_m12squared = 'm12sqr'
+Type = "yuktype"
+Lambda1 = "L1"
+Lambda2 = "L2"
+Lambda3 = "L3"
+Lambda4 = "L4"
+re_Lambda5 = "L5r"
+im_Lambda5 = "L5i"
+tanbeta = "tbeta"
+re_m12squared = "m12sqr"
 
 
 def convert(IndexCol, InputFILE, OutputFILE, Seperator):
     df = pd.DataFrame()
-    if IndexCol == 'False':
+    if IndexCol == "False":
         df = pd.read_table(InputFILE, index_col=False, sep=Seperator)
     else:
         df = pd.read_table(InputFILE, index_col=int(IndexCol), sep=Seperator)
 
     frontcol = [
-            Type, Lambda1, Lambda2, Lambda3, Lambda4, re_Lambda5, im_Lambda5,
-            re_m12squared, tanbeta
+        Type,
+        Lambda1,
+        Lambda2,
+        Lambda3,
+        Lambda4,
+        re_Lambda5,
+        im_Lambda5,
+        re_m12squared,
+        tanbeta,
     ]
 
     Col = [c for c in frontcol if c in df] + [c for c in df if c not in frontcol]
     df = df[Col]
 
-    df.to_csv(OutputFILE, index=False, sep='\t')
+    df.to_csv(OutputFILE, index=False, sep="\t")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--indexcol', help='Column which stores the index of your data', default='False')
-parser.add_argument('-in', '--input', help='Input file')
-parser.add_argument('-out', '--output', help='Output file')
-parser.add_argument('-sep', '--seperator', help='Column separator of input file', default='\t')
+parser.add_argument(
+    "-i",
+    "--indexcol",
+    help="Column which stores the index of your data",
+    default="False",
+)
+parser.add_argument("-in", "--input", help="Input file")
+parser.add_argument("-out", "--output", help="Output file")
+parser.add_argument(
+    "-sep", "--seperator", help="Column separator of input file", default="\t"
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
--- sh/prepareData_CxSM.py
+++ sh/prepareData_CxSM.py
@@ -10,40 +10,38 @@ import argparse
 
 def convert(IndexCol, InputFILE, OutputFILE, Seperator):
     df = pd.DataFrame()
-    if IndexCol == 'False':
+    if IndexCol == "False":
         df = pd.read_table(InputFILE, index_col=False, sep=Seperator)
     else:
         df = pd.read_table(InputFILE, index_col=int(IndexCol), sep=Seperator)
 
-    '''
+    """
     The parameters should have the label of the corresponding parameter.
-    '''
-    v = 'v'
-    vs = 'vs'
-    va = 'va'
-    msq = 'msq'
-    lamb = 'lambda'
-    delta2 = 'delta2'
-    b2 = 'b2'
-    d2 = 'd2'
-    Reb1 = 'Reb1'
-    Imb1 = 'Imb1'
-    Rea1 = 'Rea1'
-    Ima1 = 'Ima1'
+    """
+    v = "v"
+    vs = "vs"
+    va = "va"
+    msq = "msq"
+    lamb = "lambda"
+    delta2 = "delta2"
+    b2 = "b2"
+    d2 = "d2"
+    Reb1 = "Reb1"
+    Imb1 = "Imb1"
+    Rea1 = "Rea1"
+    Ima1 = "Ima1"
 
     NoImb1 = False
     NoIma1 = False
 
     if Imb1 not in df:
-        Reb1 = 'b1'
+        Reb1 = "b1"
         NoImb1 = True
     if Ima1 not in df:
-        Rea1 = 'a1'
+        Rea1 = "a1"
         NoIma1 = True
 
-    frontcol = [
-            v, vs, va, msq, lamb, delta2, b2, d2, Reb1, Imb1, Rea1, Ima1
-    ]
+    frontcol = [v, vs, va, msq, lamb, delta2, b2, d2, Reb1, Imb1, Rea1, Ima1]
 
     for c in frontcol:
         if c not in df:
@@ -54,19 +52,26 @@ def convert(IndexCol, InputFILE, OutputFILE, Seperator):
     df = df[Col]
 
     if NoImb1:
-        df.rename(columns={Reb1: 'Reb1'}, inplace=True)
+        df.rename(columns={Reb1: "Reb1"}, inplace=True)
     if NoIma1:
-        df.rename(columns={Rea1: 'Rea1'}, inplace=True)
+        df.rename(columns={Rea1: "Rea1"}, inplace=True)
 
-    with open(OutputFILE, 'w') as file:
-        df.to_csv(file, index=True, sep='\t')
+    with open(OutputFILE, "w") as file:
+        df.to_csv(file, index=True, sep="\t")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--indexcol', help='Column which stores the index of your data', default='False')
-parser.add_argument('-in', '--input', help='Input file')
-parser.add_argument('-out', '--output', help='Output file')
-parser.add_argument('-sep', '--seperator', help='Column separator of input file', default='\t')
+parser.add_argument(
+    "-i",
+    "--indexcol",
+    help="Column which stores the index of your data",
+    default="False",
+)
+parser.add_argument("-in", "--input", help="Input file")
+parser.add_argument("-out", "--output", help="Output file")
+parser.add_argument(
+    "-sep", "--seperator", help="Column separator of input file", default="\t"
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
