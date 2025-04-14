456062521bd6e232e57f1ac98e069d6dc334f9e9
João Viana <57032457+vollous@users.noreply.github.com>
2025-04-14 11:13:35 +0100

    Fix windows path

--- tests/unittests/Test-general.cpp
+++ tests/unittests/Test-general.cpp
@@ -68,17 +68,19 @@ TEST_CASE("Checking if executables were created", "[general]")
   SKIP("BSMPTBuildExecutables set to false. No executables.");
 #endif
   const std::string build = CMAKE_RUNTIME_OUTPUT_DIRECTORY;
-  const std::string cmd   = std::string(CMAKE_RUNTIME_OUTPUT_DIRECTORY) +
-                          std::string("/BSMPT --help");
 
   std::array<char, 128> buffer;
   std::string result;
   // Open pipe to file
 
 #ifdef _WIN32
+  const std::string cmd = std::string(CMAKE_RUNTIME_OUTPUT_DIRECTORY) +
+                          std::string("\\BSMPT --help");
   std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd.c_str(), "r"),
                                                  _pclose);
 #else
+  const std::string cmd = std::string(CMAKE_RUNTIME_OUTPUT_DIRECTORY) +
+                          std::string("/BSMPT --help");
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
                                                 pclose);
 #endif
