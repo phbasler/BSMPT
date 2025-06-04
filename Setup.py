8c8a8fe6b007a2628edc526eae53ee37dc3e0da1
Philipp Basler <28863303+phbasler@users.noreply.github.com>
2025-06-04 19:54:06 +0000

    fix typo

--- Setup.py
+++ Setup.py
@@ -188,7 +188,7 @@ def conan_install(
 ):
     config_settings = [
         "tools.cmake.cmake_layout:build_folder_vars=['settings.os','settings.arch','settings.build_type']",
-        "tools.build:compiler_executabes={'c':'/opt/homebrew/opt/llvm@14/bin/clang', 'cpp': '/opt/homebrew/opt/llvm@14/bin/clang++'}"
+        "tools.build:compiler_executables={'c':'/opt/homebrew/opt/llvm@14/bin/clang', 'cpp': '/opt/homebrew/opt/llvm@14/bin/clang++'}"
     ]
 
     build_profile = get_profile(sys.platform, get_arch(), BuildMode.release, compiler)
