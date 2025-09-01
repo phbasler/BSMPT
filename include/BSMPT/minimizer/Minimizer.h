182deb8b3d423333915e07df79655d5253145847
Philipp Basler <28863303+phbasler@users.noreply.github.com>
2025-09-01 09:20:59 +0200

    Update cmaes to version 0.10.2 (#247)
    * use 0.10.2 cmaes version
    
    * Adjust find_package import
    
    * adjust define
    
    * Use official cmaes package
    
    * remove cmaes local files
    
    * try 194 compiler verson of msvc for boost
    
    * run detect

--- include/BSMPT/minimizer/Minimizer.h
+++ include/BSMPT/minimizer/Minimizer.h
@@ -33,7 +33,7 @@ const bool UseGSLDefault = true;
 /**
  * @brief UseLibCMAESDefault Use the Libcmaes minimizer in the default settings
  */
-#ifdef cmaes_FOUND
+#ifdef libcmaes_FOUND
 const bool UseLibCMAESDefault = true;
 #else
 const bool UseLibCMAESDefault = false;
